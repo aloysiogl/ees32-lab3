c1164fbd33a16ee0f3c4236f950775b9c2dc19c3
aloysio <aloysiogl@gmail.com>
2018-10-17 15:48:53 -0300

    Auto stash before merge of "master" and "origin/master"

--- preprocessing/TransitionAnalyser.py
+++ preprocessing/TransitionAnalyser.py
@@ -1,20 +1,26 @@
 import numpy as np
 
 
-class SMGenerator:
+class TransitionAnalyser:
     def __init__(self, pol_list):
-        self.set_polynomials(pol_list)
+        self.pol_list = pol_list
+        self.m = max(map(len, pol_list))
 
     def set_polynomials(self, pol_list):
         self.pol_list = pol_list
-        print(max(map(len, pol_list)))
+        self.m = max(map(len, pol_list))
 
-    def generate(self):
-        pass
+    def transition(self, curr, inp):
+        curr_array = []
+        for i in range(self.m):
+            curr_array.append(curr % 2)
+            curr = curr//2
+        print(curr_array)
+
 
 if __name__ == "__main__":
     p1 = np.array([1,0,0,1,1,1,1])
     p2 = np.array([1,1,0,1,1,0,1])
     polarr = [p1, p2]
-    gen = SMGenerator(polarr)
-
+    gen = TransitionAnalyser(polarr)
+    TransitionAnalyser.transition(0b110, 1)
