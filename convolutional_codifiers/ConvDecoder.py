fc15bf494660e24a3b9b243e95b70592a98fcc68
aloysio <aloysiogl@gmail.com>
2018-10-27 11:02:03 -0300

    Decoding first part

--- convolutional_codifiers/ConvDecoder.py
+++ convolutional_codifiers/ConvDecoder.py
@@ -1,13 +1,35 @@
 import numpy as np
 import preprocessing.TransitionAnalyser as ta
+from convolutional_codifiers.ConvEncoder import ConvEncoder
 
 
 class ConvDecoder:
     def __init__(self, table):
         self.table = table
 
-    def decode(self):
-        pass
+        assert len(table) > 0
+
+        self.chunk_size = len(table[0][0][0])
+
+    def decode(self, message):
+        # Splitting message
+        split_message = []
+        for i in range(len(message)//self.chunk_size):
+            split_message.append(message[i*self.chunk_size:self.chunk_size*(i+1)])
+
+        # Decoding
+        paths = [[] for i in range(len(self.table))]
+        weigths = [-1 for i in range(len(self.table))]
+        weigths[0] = 0
+
+        ######
+
+        ######
+
+        #####
+        #Heladio
+        #####
+
 
     def distance_transition(self, initial, trans, seq):
         output, new = self.table[initial][trans]
@@ -18,4 +40,17 @@ class ConvDecoder:
 
 
 if __name__ == '__main__':
-    pass
+    p1 = np.array([1, 1, 0, 1])
+    p2 = np.array([1, 0, 1, 1])
+    p3 = np.array([1, 1, 1, 1])
+    polarr = [p1, p2, p3]
+    gen = ta.TransitionAnalyser(polarr)
+
+    codifier = ConvEncoder(gen.table_generate(3))
+    u = [0, 0, 1, 0]
+    print(codifier.encode(u))
+    for i in gen.table_generate(3):
+        print(i)
+
+    decoder = ConvDecoder(gen.table_generate(3))
+    decoder.decode(codifier.encode(u))
