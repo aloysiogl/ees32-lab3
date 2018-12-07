ed869da755c14b2cfd17ccbcf71bb4a14aeaffc6
aloysio <aloysiogl@gmail.com>
2018-12-07 00:34:58 -0300

    Terminando e rela (falta a formatação)

--- out/plot_script.m
+++ out/plot_script.m
@@ -22,10 +22,10 @@ conv_euc2 = read('convolutional_euclidean_pol_2_ps.txt');
     
 hold on
 grid on
-x = [-5:1:10];
+x = [-5:1:16];
 %plot(x, pchip(ei_n0, normal, x))
-%plot(x, pchip(ei_n0, hamming, x))
 plot(x, polyval(polyfit(ei_n0(1:14), normal(1:14),7), x))
+plot(x, pchip(ei_n0, hamming, x))
 %plot(x, polyval(polyfit(ei_n0(1:14), hamming(1:14),3), x))
 %plot(x, pchip(ei_n0, cyclic_6x10, x))
 %plot(x, pchip(ei_n0, cyclic_7x12, x))
@@ -34,19 +34,19 @@ plot(x, polyval(polyfit(ei_n0(1:14), normal(1:14),7), x))
 %plot(x, polyval(polyfit(ei_n0(1:13), cyclic_8x14(1:13),4), x))
 %plot(x, pchip(ei_n0, cyclic_9x15, x))
 %plot(x, polyval(polyfit(ei_n0(1:12), cyclic_9x15(1:12),5), x))
-%plot(x, pchip(ei_n0, cyclic_9x16, x))
+plot(x, pchip(ei_n0, cyclic_9x16, x))
 %plot(x, pchip(ei_n0, matrix_improved_14x6, x))
 %plot(x, polyval(polyfit(ei_n0(1:14), matrix_improved_14x6(1:14),4), x))
 %plot(x, pchip(ei_n0, matrix_improved_21x9, x))
 %plot(x, pchip(ei_n0, matrix_improved_28x12, x))
-%plot(x, pchip(ei_n0, matrix_improved_35x15, x))
+plot(x, pchip(ei_n0, matrix_improved_35x15, x))
 %plot(x, pchip(ei_n0, conv_ham0, x))
-%plot(x, polyval(polyfit(ei_n0(1:9), conv_ham0(1:9),7), x))
+plot(x, polyval(polyfit(ei_n0(1:9), conv_ham0(1:9),7), x))
 %plot(x, pchip(ei_n0, conv_ham1, x))
 %plot(x, polyval(polyfit(ei_n0(1:8), conv_ham1(1:8),5), x))
 %plot(x, pchip(ei_n0, conv_ham2, x))
 %plot(x, polyval(polyfit(ei_n0(1:7), conv_ham2(1:7),4), x))
-%plot(x, pchip(ei_n0, conv_exa0, x))
+plot(x, pchip(ei_n0, conv_exa0, x))
 %plot(x, pchip(ei_n0, conv_exa1, x))
 %plot(x, polyval(polyfit(ei_n0(1:8), conv_exa1(1:8),5), x))
 %plot(x, pchip(ei_n0, conv_exa2, x))
@@ -54,17 +54,18 @@ plot(x, polyval(polyfit(ei_n0(1:14), normal(1:14),7), x))
 %plot(x, pchip(ei_n0, conv_euc0, x))
 plot(x(1:13), polyval(polyfit(ei_n0(1:7), conv_euc0(1:7),5), x(1:13)))
 %plot(x, pchip(ei_n0, conv_euc1, x))
-plot([x(1:2),x(5:10)], [pchip(ei_n0, conv_euc1, x(1:2)),polyval(polyfit(ei_n0(1:6), conv_euc1(1:6),5), x(5:10))])
+%plot([x(1:2),x(5:10)], [pchip(ei_n0, conv_euc1, x(1:2)),polyval(polyfit(ei_n0(1:6), conv_euc1(1:6),5), x(5:10))])
 %plot(x, pchip(ei_n0, conv_euc2, x))
 plot(x, polyval(polyfit(ei_n0(1:5), conv_euc2(1:5),4), x))
-xlim([-2 10])
+xlim([-2 16])
 ylim([-4 0])
-title('Comparação códigos convolucionais')
+title('Comparação entre todos os códigos desenvolvidos')
 ylabel('log(probabilidade de erro)')
 xlabel('E_i/N_0 (dB)')
 %legend('Não codificado', 'Comparação Hamming polinômio 1', 'Comparação Hamming polinômio 2', ...
 %'Comparação Hamming polinômio 3', 'Comparação Exata polinômio 1', 'Comparação Exata polinômio 2', ...
 %'Comparação Exata polinômio 3', 'Comparação Euclidiana polinômio 1', 'Comparação Euclidiana polinômio 2', ...
 %'Comparação Euclidiana polinômio 3');
-legend('Não codificado', 'Comparação Euclidiana polinômio 1', 'Comparação Euclidiana polinômio 2', ...
-'Comparação Euclidiana polinômio 3');
+legend('Não codificado', 'Código de Hamming', 'Cíclico 9x15', 'Código próprio 15x35' ...
+, 'Comparação Hamming polinômio 1', 'Comparação Exata polinômio 1', ...
+'Comparação Euclidiana polinômio 1');
