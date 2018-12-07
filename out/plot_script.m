be24c993405d5ad852fa93f18764328cbbed7b77
aloysio <aloysiogl@gmail.com>
2018-12-06 22:45:14 -0300

    Adicionada figura com comparação dos códigos cíclicos

--- out/plot_script.m
+++ out/plot_script.m
@@ -1,7 +1,11 @@
 ei_n0 = read('ei_n0.txt');
 normal = read('normal_ps.txt');
 hamming = read('hamming_ps.txt');
+cyclic_6x10 = read('cyclicCíclico {6, 10}_ps.txt');
+cyclic_7x12 = read('cyclicCíclico {7, 12}_ps.txt');
 cyclic_8x14 = read('cyclicCíclico {8, 14}_ps.txt');
+cyclic_9x15 = read('cyclicCíclico {9, 15}_ps.txt');
+cyclic_9x16 = read('cyclicCíclico {9, 16}_ps.txt');
 matrix_improved_14x6 = read('matrix_improved_14x6');
 matrix_improved_21x9 = read('matrix_improved_21x9');
 matrix_improved_28x12 = read('matrix_improved_28x12');
@@ -9,18 +13,46 @@ matrix_improved_35x15 = read('matrix_improved_35x15');
 conv_ham0 = read('convolutional_hamming_pol_0_ps.txt');
 conv_ham1 = read('convolutional_hamming_pol_1_ps.txt');
 conv_ham2 = read('convolutional_hamming_pol_2_ps.txt');
-
+conv_exa0 = read('convolutional_exact_pol_0_ps.txt');
+conv_exa1 = read('convolutional_exact_pol_1_ps.txt');
+conv_exa2 = read('convolutional_exact_pol_2_ps.txt');
+conv_euc0 = read('convolutional_euclidean_pol_0_ps.txt');
+conv_euc1 = read('convolutional_euclidean_pol_1_ps.txt');
+conv_euc2 = read('convolutional_euclidean_pol_2_ps.txt');
+    
 hold on
+grid on
 x = [-5:1:10];
-plot(x, pchip(ei_n0, normal, x))
-plot(x, pchip(ei_n0, hamming, x))
-plot(x, pchip(ei_n0, cyclic_8x14, x))
-plot(x, pchip(ei_n0, matrix_improved_14x6, x))
-plot(x, pchip(ei_n0, matrix_improved_21x9, x))
-plot(x, pchip(ei_n0, matrix_improved_28x12, x))
-plot(x, pchip(ei_n0, matrix_improved_35x15, x))
-plot(x, pchip(ei_n0, conv_ham0, x))
-plot(x, pchip(ei_n0, conv_ham1, x))
-plot(x, pchip(ei_n0, conv_ham2, x))
-legend('Não codificado', 'Hamming', 'improved\_14x6', ...
-'improved\_21x9', 'improved\_28x12', 'improved\_35x15')
+%plot(x, pchip(ei_n0, normal, x))
+%plot(x, pchip(ei_n0, hamming, x))
+plot(x, polyval(polyfit(ei_n0(1:14), normal(1:14),7), x))
+%plot(x, polyval(polyfit(ei_n0(1:14), hamming(1:14),3), x))
+plot(x, pchip(ei_n0, cyclic_6x10, x))
+%plot(x, pchip(ei_n0, cyclic_7x12, x))
+plot(x, polyval(polyfit(ei_n0(1:13), cyclic_7x12(1:13),4), x))
+%plot(x, pchip(ei_n0, cyclic_8x14, x))
+plot(x, polyval(polyfit(ei_n0(1:13), cyclic_8x14(1:13),4), x))
+%plot(x, pchip(ei_n0, cyclic_9x15, x))
+plot(x, polyval(polyfit(ei_n0(1:12), cyclic_9x15(1:12),5), x))
+plot(x, pchip(ei_n0, cyclic_9x16, x))
+%plot(x, pchip(ei_n0, matrix_improved_14x6, x))
+%plot(x, pchip(ei_n0, matrix_improved_21x9, x))
+%plot(x, pchip(ei_n0, matrix_improved_28x12, x))
+%plot(x, pchip(ei_n0, matrix_improved_35x15, x))
+%plot(x, pchip(ei_n0, conv_ham0, x))
+%plot(x, pchip(ei_n0, conv_ham1, x))
+%plot(x, pchip(ei_n0, conv_ham2, x))
+%plot(x, pchip(ei_n0, conv_exa0, x))
+%plot(x, pchip(ei_n0, conv_exa1, x))
+%plot(x, pchip(ei_n0, conv_exa2, x))
+%plot(x, pchip(ei_n0, conv_euc0, x))
+%plot(x, pchip(ei_n0, conv_euc1, x))
+%plot(x, pchip(ei_n0, conv_euc2, x))
+%plot(x, polyval(polyfit(ei_n0(1:5), conv_euc2(1:5),4), x))
+xlim([-2 10])
+ylim([-4 0])
+title('Comparação códigos cíclicos')
+ylabel('log(probabilidade de erro)')
+xlabel('E_i/N_0 (dB)')
+legend('Não codificado', 'Cíclico 6x10', 'Cíclico 7x12', ...
+'Cíclico 8x14', 'Cíclico 9x15', 'Cíclico 9x16')
